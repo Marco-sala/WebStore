ab3fbeba8086a2d026077e5d24c7e5446f32c977
Hans Velazco <hansvo99@gmail.com>
2019-10-29 08:21:25 -0500

    corrigiendo errores

--- web-shop/src/main/java/com/webstore/webshop/controller/EmployeeController.java
+++ web-shop/src/main/java/com/webstore/webshop/controller/EmployeeController.java
@@ -61,7 +61,7 @@ public class EmployeeController {
         if(currentEmployee != null){
             return "maintenance";
         } else {
-            return "employee-login";
+            return "error";
         }
 
     }
